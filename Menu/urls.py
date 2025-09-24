0c47fca881659d9232e74ef1bdf319e19305334a
Geraldine Bastias <ge.bastias@duocuc.cl>
2025-09-24 10:10:43 -0300

    vista alumno
    carpeta con diferentes vistas de la seccion del alumno, revision de urls.py y views.py

--- Menu/urls.py
+++ Menu/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import Inicio
+from .views import Inicio,InicioAlumno,PerfilAlumno,EstadisticasAsignaturaAlumno,TestInterestAlumno,InformeAlumno
 
 urlpatterns = [
     path('',Inicio,name="Inicio"),
+
+    # URLS del alumno
+    path('inicio_alumno/', InicioAlumno, name="inicio_alumno"),
+    path('perfil_alumno/', PerfilAlumno, name="perfil_alumno"),
+    path('estadisticas_asignatura_alumno/', EstadisticasAsignaturaAlumno, name="estadisticas_asignatura_alumno"),
+    path('test_interest_alumno/', TestInterestAlumno, name="test_interest_alumno"),
+    path('informe_alumno/', InformeAlumno, name="informe_alumno"),
+
+
+
 ]
--- Menu/views.py
+++ Menu/views.py
@@ -7,3 +7,21 @@ from django.template import loader
 
 def Inicio(request):
     return render(request,'Menu/Inicio.html')
+
+# Vistas del alumno
+def InicioAlumno(request):
+    return render(request, 'Menu/vista_alumno/inicio_alumno.html')
+
+def PerfilAlumno(request):
+    return render(request, 'Menu/vista_alumno/perfil_alumno.html')
+
+def EstadisticasAsignaturaAlumno(request):
+    return render(request, 'Menu/vista_alumno/estadisticas_asignatura_alumno.html')
+
+def TestInterestAlumno(request):
+    return render(request, 'Menu/vista_alumno/test_interest_alumno.html')
+
+def InformeAlumno(request):
+    return render(request, 'Menu/vista_alumno/informe_alumno.html')
+
+
