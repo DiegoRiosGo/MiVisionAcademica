e9adb5580e2eba43abd738d917a5d621c393ff30
Geraldine Bastias <ge.bastias@duocuc.cl>
2025-10-03 09:35:59 -0300

    inicio. prueba 1

---
+++ Menu/static/assets/js/inicio.js
@@ -0,0 +1,17 @@
+// ================= Animacion de inicio / va en inicio.js =================
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector(".container");
+    const btnSignIn = document.getElementById("btn-sign-in");
+    const btnSignUp = document.getElementById("btn-sign-up");
+
+    // Botón "Registrarse" muestra el formulario Sign Up
+    btnSignUp.addEventListener('click', () => {
+        container.classList.add('toggle');
+    });
+
+    // Botón "Iniciar Sesión" muestra el formulario Sign In
+    btnSignIn.addEventListener('click', () => {
+        container.classList.remove('toggle');
+    });
+});
