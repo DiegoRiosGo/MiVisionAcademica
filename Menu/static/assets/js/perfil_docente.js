e02d5bc51e56f6848b61a055cd724b4fc779a769
Geraldine Bastias <ge.bastias@duocuc.cl>
2025-10-04 14:42:21 -0300

    apartado docente

--- Menu/static/assets/js/perfil_docente.js
+++ Menu/static/assets/js/perfil_docente.js
@@ -1,18 +1,72 @@
-// ================= Foto de Perfil / va en perfil_docente y perfil_alumno para subir foto de perfil =================
-const newProfile = document.getElementById('newProfile');
-const profileImg = document.getElementById('profileImg');
-const sidebarProfiles = document.querySelectorAll('#sidebarProfile');
+/* -------------------------------------------------------------------------------------------------------------
+   -------------------------------------- INICIO perfil_docente .JS --------------------------------------------
+   ------------------------------------------------------------------------------------------------------------- */
 
-if(newProfile){
-    newProfile.addEventListener('change', () => {
-        const file = newProfile.files[0];
-        if(file){
-            const reader = new FileReader();
-            reader.onload = function(e){
-                profileImg.src = e.target.result;
-                sidebarProfiles.forEach(img => img.src = e.target.result);
-            }
-            reader.readAsDataURL(file);
-        }
+// TITULO BODY
+
+    // SIN CODIGO
+
+// TITULO BARRA LATERAL
+
+    //personalizacion de tabs
+    document.addEventListener('DOMContentLoaded', function () {
+    const tabButtons = document.querySelectorAll('.personal-tab-btn');
+    const tabSections = document.querySelectorAll('.personal-tab-section');
+
+    tabButtons.forEach(btn => {
+        btn.addEventListener('click', () => {
+        // Quitar clase 'active' de todos los botones
+        tabButtons.forEach(b => b.classList.remove('active'));
+        // Activar el botón clickeado
+        btn.classList.add('active');
+
+        // Mostrar la sección correspondiente
+        const target = btn.dataset.target;
+        tabSections.forEach(sec => {
+            sec.style.display = (sec.id === target) ? 'block' : 'none';
+        });
+        });
     });
-}
+    });
+
+// TITULO PERFIL
+
+    // SIN CODIGO
+
+// TITULO FOTO DE PERFIL
+    const newProfile = document.getElementById('newProfile');
+    const profileImg = document.getElementById('profileImg');
+    const sidebarProfiles = document.querySelectorAll('#sidebarProfile');
+    const confirmationPopup = document.getElementById('confirmationPopup');
+
+    if(newProfile){
+        newProfile.addEventListener('change', () => {
+            const file = newProfile.files[0];
+            if(file){
+                const reader = new FileReader();
+                reader.onload = function(e){
+                    profileImg.src = e.target.result;
+                    sidebarProfiles.forEach(img => img.src = e.target.result);
+
+// TITULO POPUP    
+                    // Mostrar el popup
+                    confirmationPopup.style.display = 'block';
+
+                    // Ocultar después de 3 segundos
+                    setTimeout(() => {
+                    confirmationPopup.style.display = 'none';
+                    }, 3000);
+                };
+                reader.readAsDataURL(file);
+            }
+        });
+    }
+
+// TITULO INFORMACION PERSONAL
+
+    // SIN CODIGO    
+
+
+/* --------------------------------------------------------------------------------------------------------------
+   ---------------------------------------- FIN perfil_docente .JS ----------------------------------------------
+   -------------------------------------------------------------------------------------------------------------- */    
