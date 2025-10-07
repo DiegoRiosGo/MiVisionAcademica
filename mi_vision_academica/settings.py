8761652ba080263432f2cf354a22fd906ec00a36
DiegoRiosGo <die.rios@duocuc.cl>
2025-10-07 14:39:18 -0300

    sindatabase

--- mi_vision_academica/settings.py
+++ mi_vision_academica/settings.py
@@ -86,22 +86,11 @@ import os
 from decouple import config
 import dj_database_url
 
-if os.getenv("VERCEL"):
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=config("SUPABASE_DB_URL"),  # URL completa de tu DB en Supabase
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {}
 
+MIGRATION_MODULES = {
+    app: None for app in INSTALLED_APPS
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
