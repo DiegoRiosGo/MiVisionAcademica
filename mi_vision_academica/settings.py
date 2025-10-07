2aa5cf99cb643b3e32e6965dd2620ca1e35b14a3
DiegoRiosGo <die.rios@duocuc.cl>
2025-10-07 14:29:20 -0300

    database-1

--- mi_vision_academica/settings.py
+++ mi_vision_academica/settings.py
@@ -82,12 +82,25 @@ WSGI_APPLICATION = 'mi_vision_academica.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+import os
+from decouple import config
+import dj_database_url
+
+if os.getenv("VERCEL"):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=config("SUPABASE_DB_URL"),  # URL completa de tu DB en Supabase
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
