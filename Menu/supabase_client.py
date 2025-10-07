1dc13e1698608fc42d57788407a4e903e4a73813
DiegoRiosGo <die.rios@duocuc.cl>
2025-10-07 15:13:47 -0300

    prueba7

--- Menu/supabase_client.py
+++ Menu/supabase_client.py
@@ -5,3 +5,13 @@ SUPABASE_URL = config("SUPABASE_URL")
 SUPABASE_KEY = config("SUPABASE_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+
+# Probar conexión con una tabla existente
+try:
+    response = supabase.table("usuario").select("*").limit(1).execute()
+    print("✅ Conexión exitosa a Supabase.")
+    print("Resultado de prueba:", response.data)
+except Exception as e:
+    print("❌ Error al conectar con Supabase:")
+    print(e)
--- mi_vision_academica/settings.py
+++ mi_vision_academica/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,13 +87,15 @@ from decouple import config
 import dj_database_url
 
 
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('SUPABASE_DB_URL'),
+        default=config('SUPABASE_DB_URL'),
         conn_max_age=600,
         ssl_require=True
     )
 }
+
 
 MIGRATION_MODULES = {
     app: None for app in INSTALLED_APPS
@@ -140,4 +142,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
