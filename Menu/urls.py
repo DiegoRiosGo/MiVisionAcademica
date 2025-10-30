cf139e43aafbf29dac483b335b008b8fbfa3ea26
DiegoRiosGo <die.rios@duocuc.cl>
2025-10-30 15:41:07 -0300

    soli2

--- Menu/urls.py
+++ Menu/urls.py
@@ -41,6 +41,4 @@ urlpatterns = [
 
     #URL solicitud de retroalimentación
     path("enviar_solicitud/", views.enviar_solicitud, name="enviar_solicitud"),
-    path("obtener_notificaciones_docente/", views.obtener_notificaciones_docente, name="obtener_notificaciones_docente"),
-    path("enviar_retroalimentacion/", views.enviar_retroalimentacion, name="enviar_retroalimentacion"),
 ] 
