126298113396d2714e804a673cb7c6b2e8249d70
DiegoRiosGo <die.rios@duocuc.cl>
2025-11-05 16:01:18 -0300

    eliminar3

--- Menu/urls.py
+++ Menu/urls.py
@@ -43,6 +43,6 @@ urlpatterns = [
     path("enviar_solicitud/", views.enviar_solicitud, name="enviar_solicitud"),
     path("obtener_notificaciones_docente/", views.obtener_notificaciones_docente, name="obtener_notificaciones_docente"),
     path("enviar_retroalimentacion/", views.enviar_retroalimentacion, name="enviar_retroalimentacion"),
-    path("buscar_docentes/", views.buscar_docentes, name="buscar_docentes"),eliminar_solicitud
+    path("buscar_docentes/", views.buscar_docentes, name="buscar_docentes"),
     path("eliminar_solicitud/", views.eliminar_solicitud, name="eliminar_solicitud"),
 ] 
